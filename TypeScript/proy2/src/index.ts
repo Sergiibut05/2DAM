1a42385bea7047ae7ace23352116ed7157b92dd0
Sergii <sergiibutrii555@gmail.com>
2025-09-22 21:06:17 +0200

    base de la calculadora

--- TypeScript/proy2/src/index.ts
+++ TypeScript/proy2/src/index.ts
@@ -1,10 +1,41 @@
-// src/index.ts
-function greet(name: string): void {
-    const greeting = `Hello, ${name}!`;
-    const element = document.getElementById("greeting");
-    if (element) {
-      element.innerText = greeting;
+
+const botones = document.querySelectorAll("button");
+let calc = "";
+const calculos = document.querySelector(".calcs");
+
+let result = -1;
+botones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    if (boton.dataset.type == "number" || boton.dataset.type == "operation"){
+        calc += boton.textContent;
+        console.log(calc);
+    }else if(boton.dataset.value == "AC"){
+        calc = "";
+        console.log(calc);
+    }else if (boton.dataset.value == "delete"){
+        calc = calc.substring(0, calc.length-1);
+        console.log(calc);
+    }else{
+        try{
+            result = eval(calc);
+            console.log(result);
+            calculos!.textContent = result.toString();
+        }catch (error){
+            console.error(error);
+            calculos!.textContent = "Error";
+        }finally{
+            calc = "";
+        }
+        
     }
-  }
-  
-  greet("World");
+    //Que se Actualice el .calcs
+    if(boton.dataset.value != "="){
+        calculos!.textContent = calc || "0";
+    }
+        
+
+  });
+});
+
+
+
